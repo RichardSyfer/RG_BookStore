d1ed6bb48955de92116ca0aab8e8e0ba98ae164c
RichardSyfer <deadwalker87@gmail.com>
2018-08-21 16:10:01 +0300

    Devise and ActiveAdmin + RSpec installed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
+  root to: 'books#index'
+  resources :books, only: %i[index show]
 end
